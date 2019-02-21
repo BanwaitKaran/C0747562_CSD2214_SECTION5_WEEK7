efb11ff46a3251c6502265de4f18df9c7593d971
KARAN BANWAIT <karanbanwaitsaini@gmail.com>
2019-02-21 08:42:58 -0500

    creating object in javascript

--- C0747562_CSD2214_SECTION5_WEEK7/js/main.js
+++ C0747562_CSD2214_SECTION5_WEEK7/js/main.js
@@ -1,1 +1,20 @@
-﻿// Your code here!
+﻿var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    MaxOffspring: 5,
+    noisy: true,
+    deadly: false;
+}
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    MaxOffspring: 3,
+    noisy: true,
+    deadly: true;
+}
